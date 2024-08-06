85d48ae8e2c91800d8ece2101183c75a4d3138b1
unknown <game.oh.vn@gmail.com>
2024-08-06 13:00:56 +0700

    f

---
+++ lang/en/callback.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'invalid-callback' => 'Payment error. Please retry before contacting our CS! (Err: c1)',
+    'payment-id-invalid' => 'Payment error. Please retry before contacting our CS! (Err: c2)',
+    'payment-not-approved' => 'Payment error. Please retry before contacting our CS! (Err: c3)',
+    'exception' => 'Payment error. Please retry before contacting our CS! (Err: c4)',
+    'payment-canceled' => 'Payment canceled. We always try our best to serve you anytime.'
+];
---
+++ lang/en/payment.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'trans-not-exists' => 'Payment error. Please retry before contacting our CS! (Err: p1)',
+    'config-error' => 'Payment error. Please contact our CS! (Err: 2)',
+    'payment-not-approved' => 'Payment error. Please retry before contacting our CS! (Err: p3)',
+];
